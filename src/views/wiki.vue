1991852c8c5b21e199994ac4549972a8efe35a34
Chen715 <1658137632@qq.com>
2024-02-29 14:53:04 +0800

    ui 框架配置完成

---
+++ src/views/wiki.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+import WikiHeader from'@/views/wiki/header.vue'
+import wikiaside from'@/views/wiki/wikiaside.vue'
+</script>
+
+<template>
+    <div class="common-layout">
+      <el-container>
+        <el-header>
+            <WikiHeader /> 
+
+        </el-header>
+        
+        <el-container class="wiki-layout-aside" >
+            <el-aside width="500px" >
+
+                <wikiaside /> 
+            </el-aside>
+
+            <el-main style="font-size: 30px" >
+                Main
+                <br>Main<br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br><br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br><br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br><br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br>
+                <br>Main
+                <br>Main
+                <br>Main
+                <br>Main
+                <br>
+
+            </el-main>
+            
+        </el-container>
+      </el-container>
+    </div>
+  </template>
+
+<style>
+
+</style>
